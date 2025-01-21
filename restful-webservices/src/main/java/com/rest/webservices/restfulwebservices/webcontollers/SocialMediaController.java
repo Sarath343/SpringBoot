b732562a31af8c0b89bfa0e63d8fb9e30ac7a212
Sarath KP <87646653+Sarath343@users.noreply.github.com>
2025-01-21 19:05:35 +0530

    Custom error implementation

--- restful-webservices/src/main/java/com/rest/webservices/restfulwebservices/webcontollers/SocialMediaController.java
+++ restful-webservices/src/main/java/com/rest/webservices/restfulwebservices/webcontollers/SocialMediaController.java
@@ -1,40 +1,49 @@
 package com.rest.webservices.restfulwebservices.webcontollers;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rest.webservices.restfulwebservices.daoservice.SocialMediaDao;
 import com.rest.webservices.restfulwebservices.dto.User;
 
 @RestController
 public class SocialMediaController {
-	
+
 	SocialMediaDao dao;
-	
-	
+
 	public SocialMediaController(SocialMediaDao dao) {
 		super();
 		this.dao = dao;
 	}
-	
-	
-	
-	@GetMapping(path="/users")
-	public List<User> fetchAllUsers(){
-		return dao.fetchAllUsers() ;
-		
+
+	@GetMapping(path = "/users")
+	public List<User> fetchAllUsers() {
+		return dao.fetchAllUsers();
+
 	}
-	@GetMapping (path="/users/{id}")
+
+	@GetMapping(path = "/users/{id}")
 	public User fetchAUser(@PathVariable int id) {
-		return dao.fetchAUser(id);
+		User user = dao.fetchAUser(id);
+		if (user == null)
+			throw new UserNotFoundException("id "+id);
+
+		return user;
 	}
-	@PostMapping(path="/users")
-	public void createUser(@RequestBody User user) {
-		dao.createUser(user);
+
+	@PostMapping(path = "/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = dao.createUser(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(user.getId())
+				.toUri();
+		return ResponseEntity.created(location).build(); // to return a specific http request back
 	}
 }
