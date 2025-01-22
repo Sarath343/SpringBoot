4f5f37fc6d8c0b93a4aba601d44449061a58e260
Sarath KP <87646653+Sarath343@users.noreply.github.com>
2025-01-22 22:07:20 +0530

    Updated validation exception

--- restful-webservices/src/main/java/com/rest/webservices/restfulwebservices/exception/CustomResponseEntityExceptionHandler.java
+++ restful-webservices/src/main/java/com/rest/webservices/restfulwebservices/exception/CustomResponseEntityExceptionHandler.java
@@ -2,14 +2,14 @@ package com.rest.webservices.restfulwebservices.exception;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import com.rest.webservices.restfulwebservices.webcontollers.UserNotFoundException;
 
 @ControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -26,4 +26,12 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
 	return new ResponseEntity<ErrorDetails>(errorDetails , HttpStatus.NOT_FOUND);
 	}
 	
+	public ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		StringBuilder errorMessages=new StringBuilder();
+		ex.getFieldErrors().stream().forEach(x->errorMessages.append(x.getDefaultMessage() +" , "));
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), errorMessages.toString(), request.getDescription(false));
+		
+		return new ResponseEntity(errorDetails,HttpStatus.BAD_REQUEST) ;
+	}
 }
